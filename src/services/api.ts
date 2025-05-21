a06f5b863f86297caff6d7ba38496761c145447b
JuanLema14 <andres.lema1@udea.edu.co>
2025-05-21 10:01:52 -0500

    Update API base URL to production server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://inno-sistemas-back.onrender.com/api',
 })
 
 api.interceptors.request.use(
