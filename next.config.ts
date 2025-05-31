7f163327f46d9bc94dcd18437d9344c7b98c3cd1
JuanLema14 <andres.lema1@udea.edu.co>
2025-05-31 14:11:39 -0500

    Add webpack configuration to resolve alias for src directory

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: false,
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  }
 };
 
 export default nextConfig;
